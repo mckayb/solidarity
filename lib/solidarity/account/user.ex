0feb0608d37415bbbc5cc8cfaa2f4d6481182c29
McKay Broderick <mckay.db@gmail.com>
2019-10-20 15:15:37 -0600

    Hide password from the response

--- lib/solidarity/account/user.ex
+++ lib/solidarity/account/user.ex
@@ -2,6 +2,7 @@ defmodule Solidarity.Account.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @derive {Jason.Encoder, only: [:email, :name, :username, :uuid]}
   schema "users" do
     field :email, :string
     field :name, :string
